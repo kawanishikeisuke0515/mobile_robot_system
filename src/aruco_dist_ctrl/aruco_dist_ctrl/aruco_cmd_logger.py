f236480305e693851e489c12efe374ab94fa9809
Keisuke Kawanishi <kawanishi.keisuke@megachips.co.jp>
2026-06-10 13:40:58 -0700

    changed QoS policy

--- src/aruco_dist_ctrl/aruco_dist_ctrl/aruco_cmd_logger.py
+++ src/aruco_dist_ctrl/aruco_dist_ctrl/aruco_cmd_logger.py
@@ -11,6 +11,8 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Twist
 from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from rclpy.qos import ReliabilityPolicy
 
 
 class ArucoCmdLogger(Node):
@@ -60,12 +62,15 @@ class ArucoCmdLogger(Node):
         self.latest_optitrack_pose: Optional[PoseStamped] = None
         self.latest_cmd: Optional[Twist] = None
 
+        optitrack_qos = QoSProfile(depth=10)
+        optitrack_qos.reliability = ReliabilityPolicy.BEST_EFFORT
+
         self.create_subscription(ArucoDistance, '/aruco/distance', self.aruco_callback, 10)
         self.create_subscription(
             PoseStamped,
             self.optitrack_pose_topic,
             self.optitrack_pose_callback,
-            10,
+            optitrack_qos,
         )
         self.create_subscription(Twist, '/rov_cmd_vel', self.cmd_callback, 10)
         self.create_timer(1.0 / self.log_rate, self.log_callback)
